f7eabee9cf5523043c93d6e586a0a6f740e3b374
Nthenge <abrahamnetsec@gmail.com>
2023-06-09 21:06:55 +0300

    Second commit

--- fetch.php
+++ fetch.php
@@ -1,36 +1,41 @@
 <?php
-// Assuming you have a MySQL database, adjust the following variables with your database credentials
 $host = 'localhost';
 $dbname = 'authentication';
 $username_db = 'root';
 $password_db = '';
 
-// Create a connection to the database
 $conn = new mysqli($host, $username_db, $password_db, $dbname);
 
-// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve the form data
 $email = $_GET["email"];
 $password = $_GET["password"];
 
-// Prepare and execute the SQL query to fetch the user information based on the provided email
-$sql = "SELECT * FROM registration WHERE email = '$email' AND password = '$password'";
-$result = $conn->query($sql);
+// Validate and sanitize user input
+$email = mysqli_real_escape_string($conn, $email);
+$password = mysqli_real_escape_string($conn, $password);
+
+// Hash the password (you may need to adjust the hashing algorithm based on your requirements)
+$hashedPassword = hash('sha256', $password);
+
+// Prepare and execute the SQL query using prepared statements
+$sql = "SELECT * FROM registration WHERE email = ? AND password = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $email, $hashedPassword);
+$stmt->execute();
+$result = $stmt->get_result();
 
 if ($result->num_rows == 1) {
     // User found, proceed with login
-    // Redirect to the appropriate page or perform further actions
     header("Location: ./home.html");
 } else {
     // User not found
     header("Location: ./index.php?error=user_not_found");
 }
 
-// Close the database connection
+$stmt->close();
 $conn->close();
-
 ?>
+
--- register.php
+++ register.php
@@ -1,43 +1,46 @@
 <?php
-// Assuming you have a MySQL database, adjust the following variables with your database credentials
 $host = 'localhost';
 $dbname = 'authentication';
 $username_db = 'root';
 $password_db = '';
 
-// Create a connection to the database
 $conn = new mysqli($host, $username_db, $password_db, $dbname);
 
-
-// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve the form data
     $name = $_POST["name"];
     $email = $_POST["email"];
     $password = $_POST["password"];
 
+    // Validate and sanitize user input
+    $name = mysqli_real_escape_string($conn, $name);
+    $email = mysqli_real_escape_string($conn, $email);
+
+    // Hash the password
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    // Prepare and execute the SQL query to insert the data into the database
-    $sql = "INSERT INTO registration (name, email, password) VALUES ('$name', '$email', '$hashedPassword')";
+    // Prepare and execute the SQL query using prepared statements
+    $sql = "INSERT INTO registration (name, email, password) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $name, $email, $hashedPassword);
+    $stmt->execute();
 
-    if ($conn->query($sql) === TRUE) {
-        echo "<script>";
-        echo "document.getElementById('myForm').reset();";
-        echo "</script>";
-        header("Location: login.php"); 
+    if ($stmt->affected_rows == 1) {
+        // Success, redirect to login page
+        header("Location: /login.php");
     } else {
-        header("Location: index.php"); 
+        // Failed, redirect to index page or display an error message
+        header("Location: ./index.php");
     }
+
+    $stmt->close();
 }
 
-// Close the database connection
 $conn->close();
 ?>
+
 
 
